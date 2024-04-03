29d25fc9346b87f1bf0443eb476a497176c74a63
YasminTaye55 <165961167+YasminTaye55@users.noreply.github.com>
2024-04-04 00:52:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+//Define input Parameters
+let ma_period = 10;
+let bb_period =20;
+let bb_deviation =2;
+let rsi_period =14;
+let buy_tp =80;
+let buy_s1 =50;
+let sell_tp =80;
+let buy_rsi_max =81;
+let sell_rsi_min =24;
+
+//Define necessary variables 
+//double ma_period, upper_bb, lower_bb;
+//int rsi_value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
